1bef72547760207cdd0dd8dc2577a43e40a15d59
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2025-01-07 12:04:57 +0100

    css suite fin carousel

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,17 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuClose = document.querySelector(".menu-close");
     const menu = document.getElementById("menu");
 
-    // Ouvrir le menu
     menuToggle.addEventListener("click", () => {
         menu.setAttribute("aria-expanded", "true");
     });
 
-    // Fermer le menu
+
     menuClose.addEventListener("click", () => {
         menu.setAttribute("aria-expanded", "false");
     });
 
-    // Fermer le menu si on clique en dehors
+
     document.addEventListener("click", (event) => {
         if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
             menu.setAttribute("aria-expanded", "false");
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,46 @@
 
+const slides = document.querySelectorAll('.carousel-slide'); 
+const prevButton = document.querySelector('.carousel-button.prev'); 
+const nextButton = document.querySelector('.carousel-button.next'); 
+let currentSlide = 0; 
+
+
+function updateSlides() {
+    
+    slides.forEach((slide, index) => {
+        slide.style.display = 'none'; 
+    });
+
+
+    slides[currentSlide].style.display = 'block';
+
+    
+    if (currentSlide === 0) {
+        prevButton.disabled = true; 
+    } else {
+        prevButton.disabled = false; 
+    }
+
+    if (currentSlide === slides.length - 1) {
+        nextButton.disabled = true; 
+    } else {
+        nextButton.disabled = false; 
+    }
+}
+
+prevButton.addEventListener('click', () => {
+    if (currentSlide > 0) {
+        currentSlide--; 
+        updateSlides(); 
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    if (currentSlide < slides.length - 1) {
+        currentSlide++; 
+        updateSlides(); 
+    }
+});
+
+
+updateSlides(); 
